4a31790c0a803aca367f40b3012e84c1afd92afa
mohdfaw <muhammedfawaz@student.uts.edu.au>
2020-05-06 00:01:50 +1000

    Weapon Settings for Standard Rifle + Prefab Projectile Shooting + Raycast Shooting

---
+++ Assets/Scripts/Player/PrefabShooting.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class PrefabShooting : MonoBehaviour
+{
+    public GameObject bullet;
+
+    WeaponStats standardrifleStats;
+
+    void Start()
+    {
+        standardrifleStats = GameManager.Instance.gameSettings.Weapons.Where(x => x.weaponName == "StandardRifle").First().Stats;
+    }
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject instBullet = Instantiate(bullet, transform.position, transform.rotation) as GameObject;
+            Rigidbody instBulletRigidbody = instBullet.GetComponent<Rigidbody>();
+            instBulletRigidbody.AddForce(Vector3.forward * standardrifleStats.shotSpeed);
+            Destroy(instBullet, 3f);
+        }
+    }
+}
