1ff09fc634743cd500bdd3a505f67829a6677f31
Mitchell Sanderson <mitchell.titan@gmail.com>
2020-05-08 15:18:09 +1000

    Gave the Gunner the ability to shoot

---
+++ Assets/Scripts/Enemies/EnemyHeavyGunner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHeavyGunner : MonoBehaviour
+{
+    Transform targetTransform;
+
+    void Start()
+    {
+        targetTransform = GameManager.Instance.playerController.transform;
+    }
+
+    void Update()
+    {
+        transform.LookAt(targetTransform);
+    }
+}
