e5657c2d1d266e1f33390255bea55503f5b7a406
Kyle Hammer <kylethomashammer@gmail.com>
2020-05-12 14:34:15 +1000

    HP Canvas Implemented
    HP visual side implemented, still requires hooking up to enemy collisions

---
+++ Assets/Scripts/UI/Health.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public int maxHealth = 100;
+    private int currentHealth;
+
+    //= delegate to avoid null reference exception
+    public event Action<float> OnHealthPercentChange = delegate { };
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    void Update()
+    {
+        //For testing purposes, we're using space to modify health
+        if (Input.GetKeyDown(KeyCode.Space))
+            ModifyHealth(-10);
+    }
+
+    //Add or subrtract current health, update the percent
+    public void ModifyHealth(int amount)
+    {
+        currentHealth += amount;
+
+        float currentHealthPercent = (float)currentHealth / (float)maxHealth;
+        OnHealthPercentChange(currentHealthPercent);
+    }
+    
+}
