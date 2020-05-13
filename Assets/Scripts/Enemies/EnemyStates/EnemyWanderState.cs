87700173e46e7d1984001aa8db4262830feb9af5
Mitchell Sanderson <mitchell.titan@gmail.com>
2020-05-13 13:51:14 +1000

    Enemy State Machine Started

---
+++ Assets/Scripts/Enemies/EnemyStates/EnemyWanderState.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyWanderState : EnemyState
+{
+    private int randomX;
+    private int randomZ;
+
+    public override void BeginState()
+    {
+
+    }
+}
