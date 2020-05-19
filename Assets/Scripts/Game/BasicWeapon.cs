906b6ac644b289d66c7b56bcb3336cb54f1d0961
mohdfaw <muhammedfawaz@student.uts.edu.au>
2020-05-20 03:46:15 +1000

    Fire Types - Spread and Burst

--- Assets/Scripts/Game/BasicWeapon.cs
+++ Assets/Scripts/Game/BasicWeapon.cs
@@ -11,17 +11,26 @@ public class BasicWeapon : MonoBehaviour
 
     bool canShoot = true;
 
+    List<Quaternion> bullets;
+
+    void Start()
+    {
+        bullets = new List<Quaternion>(weaponStats.numBullets);
+        for (int i = 0; i < weaponStats.numBullets; i++)
+        {
+            bullets.Add(Quaternion.Euler(Vector3.zero));
+        }
+    }
+
     public void Shoot() {
-        if (canShoot) {
-            BasicProjectile currentBullet = GameObject.Instantiate(GameManager.Instance.ProjectilePrefab, firePoint).GetComponent<BasicProjectile>();
-            currentBullet.transform.parent = GameManager.Instance.transform;
-            currentBullet.owningObject = this.gameObject;
-            currentBullet.range = weaponStats.range;
-            currentBullet.damageAmount = weaponStats.weaponDamage;
-
-            currentBullet.GetComponent<Rigidbody>().velocity = this.gameObject.transform.forward * weaponStats.shotSpeed;
-
-            StartCoroutine(Reload());
+        switch((int)fireType)
+            {
+                case 0: singleShoot();
+                        break;
+                case 1: spreadShoot();
+                        break;
+                case 2: burstShoot();
+                        break;
         }
     }
 
@@ -36,4 +45,59 @@ public class BasicWeapon : MonoBehaviour
 
         canShoot = true;
     }
-}
+
+    void singleShoot()
+    {
+        if (canShoot)
+        {
+            BasicProjectile currentBullet = GameObject.Instantiate(GameManager.Instance.ProjectilePrefab, firePoint).GetComponent<BasicProjectile>();
+            currentBullet.transform.parent = GameManager.Instance.transform;
+            currentBullet.owningObject = this.gameObject;
+            currentBullet.range = weaponStats.range;
+            currentBullet.damageAmount = weaponStats.weaponDamage;
+
+            currentBullet.GetComponent<Rigidbody>().velocity = this.gameObject.transform.forward * weaponStats.shotSpeed;
+
+            StartCoroutine(Reload());
+        }
+    }
+
+    void spreadShoot()
+    {
+        if (canShoot)
+        {
+            for (int i = 0; i < weaponStats.numBullets; i++)
+            {
+                bullets[i] = Random.rotation;
+                BasicProjectile currentBullet = GameObject.Instantiate(GameManager.Instance.ProjectilePrefab, firePoint).GetComponent<BasicProjectile>();
+                currentBullet.transform.parent = GameManager.Instance.transform;
+                currentBullet.transform.rotation = Quaternion.RotateTowards(currentBullet.transform.rotation, bullets[i], 10);
+                currentBullet.owningObject = this.gameObject;
+                currentBullet.range = weaponStats.range;
+                currentBullet.damageAmount = weaponStats.weaponDamage;
+
+                currentBullet.GetComponent<Rigidbody>().velocity = currentBullet.gameObject.transform.forward * weaponStats.shotSpeed;
+                StartCoroutine(Reload());
+            }
+            
+        }
+    }
+
+    void burstShoot()
+    {
+        if (canShoot)
+        {
+            for (int i = 0; i < weaponStats.numBullets; i++)
+            {
+                BasicProjectile currentBullet = GameObject.Instantiate(GameManager.Instance.ProjectilePrefab, firePoint).GetComponent<BasicProjectile>();
+                currentBullet.transform.parent = GameManager.Instance.transform;
+                currentBullet.owningObject = this.gameObject;
+                currentBullet.range = weaponStats.range;
+                currentBullet.damageAmount = weaponStats.weaponDamage;
+
+                currentBullet.GetComponent<Rigidbody>().velocity = this.gameObject.transform.forward * weaponStats.shotSpeed;                
+            }
+            StartCoroutine(Reload());
+        }
+    }
+}   
