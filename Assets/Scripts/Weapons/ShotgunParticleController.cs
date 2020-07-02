c29ce430ad90580c6eb5e959635ac64c73669b4b
Kyle Hammer <kylethomashammer@gmail.com>
2020-07-03 07:54:19 +1000

    Shotgun Tweaks
    Shotguns no longer hit allies. Shotguns can now crit

--- Assets/Scripts/Weapons/ShotgunParticleController.cs
+++ Assets/Scripts/Weapons/ShotgunParticleController.cs
@@ -16,6 +16,9 @@ public class ShotgunParticleController : MonoBehaviour
 
         if (other.GetComponent<IDamageable>() != null)
         {
+            //Responsible to preventing enemy fire
+            if(owningObject.GetComponent<BasicEnemy>() != null && other.gameObject.GetComponent<BasicEnemy>()) return;
+
             DamageType damage;
             damage.owningObject = owningObject;
             damage.impactPosition = other.transform.position;
@@ -23,6 +26,13 @@ public class ShotgunParticleController : MonoBehaviour
             damage.damageAmount = this.damageAmount;
             damage.isCrit = false;
             damage.isPiercing = false;
+
+            if(owningObject.GetComponent<BasicPlayer>() !=null)
+            {
+                BasicPlayer player = owningObject.GetComponent<BasicPlayer>();
+                if(Random.Range(0, 101) <= player.statHandler.CritChance)
+                    damage.isCrit = true;
+            }
 
             other.GetComponent<IDamageable>().OnReceivedDamage(damage, other.transform.position, damage.impactVelocity.normalized, damage.impactVelocity.magnitude);
         }
